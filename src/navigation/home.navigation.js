97fea82f138ba05eeb83dd50edc1a2ab8f542b6d
Sudin <sudinsth1997@gmail.com>
2021-06-20 16:20:58 +0545

    Navigations Added

---
+++ src/navigation/home.navigation.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ToDoApp from '../ToDoApp';
+import { Header } from '../component/header';
+import {HomeScreen} from '../screens/main.screen';
+import ShowItem from '../screens/showItems';
+
+const Stack = createStackNavigator();
+
+export const AppNavigation = () => {
+    return (
+       <NavigationContainer>
+           <Stack.Navigator>
+               <Stack.Screen 
+                name="Home"
+                component={HomeScreen}
+                options={{headerShown: false}}
+               />
+               <Stack.Screen 
+                name="AddItem"
+                component={ToDoApp}
+                options={{
+                    headerTitle: () => (<Header />),
+                    headerStyle: {  
+                        height: 110,                  
+                    },
+                    
+                }}
+               />
+               <Stack.Screen 
+                name="ShowItem"
+                component={ShowItem}
+                options={{
+                    headerTitle: () => (<Header />),
+                    headerStyle: {  
+                        height: 110,                  
+                    },
+                    
+                }}
+               />
+           </Stack.Navigator>
+       </NavigationContainer>
+    );
+};
