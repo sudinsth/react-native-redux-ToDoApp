626ca6dcb4fdb455c3fdcf68cb2a837d6a45879e
Sudin <sudinsth1997@gmail.com>
2021-07-05 15:37:32 +0545

    Custom Drawer Contents

--- src/navigation/drawer.navigation.js
+++ src/navigation/drawer.navigation.js
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { HomeStack } from "./home.navigation";
 
+import { DrawerContent } from '../screens/DrawerContent';
 import { ShowCompletedScreen } from '../screens/showItems';
 import { ShowNotCompletedScreen } from '../screens/showItems';
 
@@ -11,7 +12,7 @@ const Drawer = createDrawerNavigator();
 export const AppNavigation = () => {
     return (
         <NavigationContainer>
-            <Drawer.Navigator>
+            <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
                 <Drawer.Screen name="All Tasks" component={HomeStack}/>
                 <Drawer.Screen name="Completed" component={ShowCompletedScreen}/>
                 <Drawer.Screen name="Not Completed" component={ShowNotCompletedScreen}/>
