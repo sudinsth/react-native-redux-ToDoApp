8f6d885eb815e5e6159b8e8a06e96c3960b8c5e0
Sudin <sudinsth1997@gmail.com>
2021-07-05 14:51:29 +0545

    Custom TextInput

---
+++ src/component/LabelInput.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { 
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+} from 'react-native';
+
+import { colors } from '../constants/color';
+
+export default ({
+    placeholder,
+    errorMessage, 
+    inputStyle, 
+    text, 
+    onChangeText,
+    ...inputProps
+}) => {
+    return(
+        <View style={{flex: 1, marginVertical: 30}}>
+            <View style={{backgroundColor: 'yellow'}}>
+                <Text style={{color: colors.red}}>
+                    {errorMessage && `*${errorMessage}`}
+                </Text>
+            </View>
+                <TextInput 
+                    placeholder={placeholder}
+                    style={[styles.input, inputStyle]}
+                    value={text}
+                    onChangeText={onChangeText}
+                    {...inputProps}
+                />
+        </View>
+    );
+}
+
+const styles=StyleSheet.create({
+    input: {
+        height: 40,
+        fontSize: 16,
+        color: '#333',
+        padding: 10,
+        marginVertical: 12,
+        borderColor: 'grey',
+        borderWidth: 1
+    },
+});
